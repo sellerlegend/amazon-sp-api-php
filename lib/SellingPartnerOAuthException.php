795bd58f804ac31f9b0ab726d6d0e6613f39bf78
Taskeen Fatima <taskeen.fatima@ymail.com>
2022-04-15 11:15:09 +0500

    Fixed: Wrong parameters for SellingPartnerOAuthException

--- lib/SellingPartnerOAuthException.php
+++ lib/SellingPartnerOAuthException.php
@@ -7,6 +7,11 @@ use Throwable;
 
 class SellingPartnerOAuthException extends Exception {
     public function __construct($message = '', $code = 0, Throwable $previous = null) {
+        if (!is_numeric($code)) {
+            $message = '[' . $code . '] ' . $message;
+            $code = 0;
+        }
+
         parent::__construct($message, $code, $previous);
     }
 }
