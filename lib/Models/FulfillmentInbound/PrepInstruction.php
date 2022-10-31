8c2ca7ac3321e0f3edb15e405bc17b745675f471
Taskeen Fatima <taskeen.fatima@ymail.com>
2022-10-31 16:03:29 +0500

    Fixed FulfillmentInbound/PrepInstruction response

--- lib/Models/FulfillmentInbound/PrepInstruction.php
+++ lib/Models/FulfillmentInbound/PrepInstruction.php
@@ -25,6 +25,9 @@ namespace SellerLegend\AmazonSellingPartnerAPI\Models\FulfillmentInbound;
  * @author   Stefan Neuhaus / ClouSale
  */
 class PrepInstruction {
+
+    public $value;
+
     /**
      * Possible values of this enum.
      */
@@ -34,6 +37,11 @@ class PrepInstruction {
     const BLACK_SHRINK_WRAPPING = 'BlackShrinkWrapping';
     const LABELING = 'Labeling';
     const HANG_GARMENT = 'HangGarment';
+    const SET_CREATION = 'SetCreation';
+    const BOXING = 'Boxing';
+    const SUFFOCATION_STICKERING = 'SuffocationStickering';
+    const SET_STICKERING = 'SetStickering';
+    const REMOVE_FROM_HANGER = 'RemoveFromHanger';
 
     /**
      * Gets allowable values of the enum.
@@ -47,6 +55,29 @@ class PrepInstruction {
             self::TAPING,
             self::BLACK_SHRINK_WRAPPING,
             self::LABELING,
-            self::HANG_GARMENT,];
+            self::HANG_GARMENT,
+            self::SET_CREATION,
+            self::BOXING,
+            self::SUFFOCATION_STICKERING,
+            self::SET_STICKERING,
+            self::REMOVE_FROM_HANGER,
+        ];
+    }
+
+    public function __construct($value) {
+        if (is_null($value) || !in_array($value, self::getAllowableEnumValues())) {
+            throw new \InvalidArgumentException(sprintf("Invalid value for enum 'PrepInstruction', must be one of '%s'", implode("', '", self::getAllowableEnumValues())));
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * Convert the enum value to a string.
+     *
+     * @return string
+     */
+    public function __toString() {
+        return $this->value;
     }
 }
